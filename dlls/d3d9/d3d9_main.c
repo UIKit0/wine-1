tizbac/wine

d3d9: register fake class correctly

dlls/d3d9/d3d9_main.c
@@ -148,9 +148,17 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
 {
     /* At process attach */
     TRACE("fdwReason=%d\n", fdwReason);
-    if (fdwReason == DLL_PROCESS_ATTACH)
-        DisableThreadLibraryCalls(hInstDLL);
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            d3dadapter9_init(hInstDLL);
+            DisableThreadLibraryCalls(hInstDLL);
+            break;
 
+        case DLL_PROCESS_DETACH:
+            d3dadapter9_destroy(hInstDLL);
+            break;
+    }
     return TRUE;
 }
 

UIKit0/wine-1

dlls/d3d9/d3d9_main.c
@@ -146,8 +146,17 @@ void* WINAPI Direct3DShaderValidatorCreate9(void)
  */
 BOOL WINAPI DllMain(HINSTANCE inst, DWORD reason, void *reserved)
 {
-    if (reason == DLL_PROCESS_ATTACH)
-        DisableThreadLibraryCalls(inst);
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+            d3dadapter9_init(inst);
+            DisableThreadLibraryCalls(inst);
+            break;
+
+        case DLL_PROCESS_DETACH:
+            d3dadapter9_destroy(inst);
+            break;
+    }
 
     return TRUE;
 }